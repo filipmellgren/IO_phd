8b374419a6a0421feda1d4c004ca4dcada0a98a9
Filip Mellgren <filip.mellgren@gmail.com>
2022-11-09 09:01:15 +0100

    up to 4 complete, 5,6, started

--- empirical_ps/empio_main.py
+++ empirical_ps/empio_main.py
@@ -1,17 +1,17 @@
 # Empirical problem set Industrial Organizatino 2022. Filip Mellgren
 # filip.mellgren@su.se
-
-
 # TODO: read: https://pyblp.readthedocs.io/en/stable/
 # See my old Berry 1994 estimation: https://github.com/filipmellgren/IO/blob/master/Workshop_3/Workshop_3.Rmd
 
 import pandas as pd
 from scipy import stats
 from sklearn.linear_model import LinearRegression
+import numpy as np
+from plotting import plot_all
+import ipdb
 import plotly.express as px
-import plotly.io as pio
+from sklearn import datasets, linear_model
 import pyblp
-pio.templates.default = "plotly_white"
 
 # Store x time is a 'market'
 # What about outside option here?
@@ -22,36 +22,113 @@ df = pd.read_excel("data/wine_ps2.xls")
 df = df.dropna()
 
 # 1. Descriptive statistics
+# Number of products
+plot_all(df)
 
-df.describe()
-
-dagg = df.groupby(["date", "storenum"]).sum().reset_index()
-fig = px.line(dagg, x='date', y="numbot", color = "storenum")
-fig.show()
-
-
+# Based on plots, do some data filtration
+df["price"] = df["p"] * df["fx"]
+df["lnprice"] = np.log(df["price"])
+df = df[df["lnprice"] < 6]
 
 # 2. Estimate demand for wine
 # Problem with this specification: prices are endogenous
 # Additional issue: how seaprated are the markets? Some SUTVA might be violated
-top_N = 5
-top_wines = df.groupby("winespec").sum()["numbot"].sort_values(ascending = False).reset_index()[:top_N].winespec
-df = df.loc[pd.Series(df.winespec).isin(top_wines)]
+top_N = 4
+top_wines = df.groupby("idcode").sum()["numbot"].sort_values(ascending = False).reset_index()[:top_N].idcode
+df = df.loc[pd.Series(df.idcode).isin(top_wines)]
 
+df_prices = df.pivot(index=["date", "storenum"], columns = "idcode", values='lnprice')
 
-X = df[["p", "pinot"]] # TODO: add price of competing products (price other wines same store same week?)
-Y = df["numbot"]
-reg = LinearRegression().fit(X, Y)
+df = df.set_index(["date", "storenum"])
+df = df.join(df_prices)
+df = df.dropna()
+df["lnq"] = np.log(df["numbot"])
+
+df["lnq"].describe()
+df["lnprice"].describe()
+
+df["Wine_id"] = df["idcode"].astype(str)
+fig = px.scatter(df, x = "lnq", y = "lnprice", color = "Wine_id", trendline = "ols")
+fig.show()
+fig.write_image("figures/price_quant.png")
+
+els = []
+for wine in top_wines:
+	X = df.loc[df.idcode == wine, df.columns.isin(top_wines)]
+	Y = df.loc[df.idcode == wine, "lnq"]
+	reg = LinearRegression().fit(X, Y)
+	els.append(reg.coef_)
+
+els = pd.DataFrame(els)
+els.index = top_wines
+els.index = els.index.map(str)
+els.columns = top_wines
+els.columns = els.columns.map(str)
+
+fig = px.imshow(els, title = "Elasticity estimates under OLS controlling for price") 
+fig.write_image("figures/elasticities_ols.png")
 
 # 3. Berry 1994, Estimation. Estimate demand using discrtete choice a la Berry (1994) using observable product characteristics
+Msize = df["numbot"].groupby(level = [0,1]).sum().groupby(level = [1]).max()*3 # Max bought over time, multiuplied by constant
+df = df.join(Msize, rsuffix = "_market")
+df["mshare"] = df["numbot"] / df["numbot_market"]
+df["mshare_oo"] = 1 - df.mshare.groupby(level = [0,1]).agg("sum")
+df["delta_j"] = np.log(df["mshare"]) - np.log(df["mshare_oo"])
 
-# 4. Berry 1994, Markups. Implied markups assuming MC are constant.
+def berry94_est(X, Y):
+	''' Estimate alpha in Equation 14 in Berry 1994.
+	Assumes there is a variable "lnprice" which is log(price).
+	'''
+	price_var_loc = np.where(X.columns == "lnprice")[0][0]
+	reg = LinearRegression().fit(X, Y) # Estimate beta and alpha through eq. 14 Berry 1994
+
+	alpha = reg.coef_[price_var_loc]
+	return(alpha)
+
+X_vars = ["lnprice", "proof", "variet", "doc"]
+X = df.loc[:, df.columns.isin(X_vars)] 
+Y = df.loc[:, "delta_j"]
+
+b94_est = berry94_est(X, Y)
+np.savetxt("figures/b94_elas.csv", np.expand_dims(b94_est,0))
+
+# 4. Berry 1994, eq 31 (slighly before I think) Implied markups assuming MC are constant.
 # https://pyblp.readthedocs.io/en/stable/_notebooks/tutorial/post_estimation.html
+def berry94_markups(price, mshare, alpha):
+	''' Back out marginal costs as in Eq 31 Berry 94
+	'''
+	mc = price - 1/(alpha * (1 - mshare))
+	markup = (price - mc)/price
+	markup_quantiles = markup.quantile([.05, 0.25, 0.5, 0.75, 0.95])
+	return(markup_quantiles)
+
+markup_quantiles = berry94_markups(df["price"] , df["mshare"], b94_est)
+np.savetxt("figures/b94_markup_pct.csv", markup_quantiles)
 
 # 5. Estimate demand again, with product fixed effects instead of observables
+# https://pyhdfe.readthedocs.io/en/stable/_notebooks/sklearn.html
+# https://pyblp.readthedocs.io/en/stable/_notebooks/tutorial/logit_nested.html#Theory-of-Plain-Logit
+df["product_ids"] = df["idcode"]
+df["prices"] = df["price"]
+df["shares"] = df["mshare"]
+df["market_ids"] = df.index.get_level_values('date').astype(str) +"_"+ df.index.get_level_values('storenum').astype(str)
+df["demand_instruments0"] = df["price"] # TODO: this subs in for an actual instrument and is needed. Is this the right way to do it?
+logit_formulation = pyblp.Formulation('prices', absorb='C(product_ids)')
+problem = pyblp.Problem(logit_formulation, df)
+# T is the number of markets
+# N is the length of the data set
+# K1 is the dimension of the linear demand parameters
+# ED is the numbe rof fixed effects dimenions, 1 is for one dim fixed effects
+logit_results = problem.solve()
 
-# 6. BLP: Sam but with BLP-instruments for price
-# Note: key differences relativey to Beryy 1994 is "Random coefficients model" of consumer utility?
-
+# 6. BLP: Same but with BLP-instruments for price
+#Create a BLP instrument, a function of characteristics of other products' features
+df["demand_instruments0"] = df["proof"].groupby(level = [0,1]).sum() - df["proof"]
+df["demand_instruments1"] = df["merlot"].groupby(level = [0,1]).sum() - df["merlot"]
+#df["demand_instruments2"] = df["doc"].groupby(level = [0,1]).sum() - df["doc"] # Colinear with current setup, add more products or just different products.
+logit_formulation = pyblp.Formulation('prices')
+problem = pyblp.Problem(logit_formulation, df)
+logit_results = problem.solve()
 
 # 7. Same but with nested logit specificataion
+# https://pyblp.readthedocs.io/en/stable/_notebooks/tutorial/logit_nested.html#Theory-of-Nested-Logit
---
+++ empirical_ps/plotting.py
@@ -0,0 +1,52 @@
+# Plots for empirical problem set
+import plotly.express as px
+import plotly.io as pio
+import pyblp
+pio.templates.default = "plotly_white"
+import pandas as pd
+import numpy as np
+
+def plot_all(df):
+
+	numbots_by_prod_tot = df[["prodname", "numbot"]].groupby(["prodname"]).sum().sort_values("numbot", ascending = False).reset_index()
+	n_prods = numbots_by_prod_tot.shape[0]
+
+	fig = px.bar(numbots_by_prod_tot, x = "prodname", y = "numbot", title = f"Long tail of wines with few sales, and a few super sellers <br><sup>Total number of products: {n_prods}</sup>").update_layout(xaxis_title = "Wine variety", yaxis_title = "Total number of bottles sold in data")
+	fig.update_xaxes(visible=True, showticklabels=False)
+	fig.write_image("figures/bottles_by_prod.png")
+
+	# Volumes
+	date_store = df[["numbot", "storenum", "date"]].\
+	groupby(["date", "storenum"]).sum().reset_index()
+
+	fig = px.box(date_store, x = "date", y = "numbot").\
+	update_layout(xaxis_title = "Date", yaxis_title = "Bottles sold")
+	
+	fig.write_image("figures/bottles_time.png")
+
+
+	# Price trend
+	df["price"] = df["p"] * df["fx"]
+	df["lnprice"] = np.log(df["price"])
+	fig = px.histogram(df, x="lnprice").\
+	update_layout(xaxis_title = "Log Price", yaxis_title = "Count", title = "Two broad price categories")
+	
+	fig.write_image("figures/log_price_hist.png")
+	df["price_cat"] = pd.cut(df.lnprice, bins = 2, labels = ["Popular", "Super premium"])
+	df["sales"] = df["price"] * df["numbot"]
+	df_time_price = df.groupby(["date", "price_cat"]).sum()
+	df_time_price["avg_price"] = df_time_price["sales"] / df_time_price["numbot"]
+
+	first_date = np.min(df_time_price.reset_index()["date"])
+	df_first_price = df_time_price[df_time_price.index.get_level_values(0) == first_date]["avg_price"].reset_index().set_index("price_cat")["avg_price"]
+	df_time_price = df_time_price.join(df_first_price, rsuffix = "_first")
+	df_time_price["avg_price_index"] = df_time_price["avg_price"]/df_time_price["avg_price_first"]
+	df_time_price = df_time_price.reset_index()
+
+	fig = px.line(df_time_price, x="date", y="avg_price_index", color='price_cat', markers = True).\
+	update_layout(xaxis_title = "Date", yaxis_title = "Paasche price index", title = "Price evolution")
+	fig.write_image("figures/price_index.png")
+	return
+
+
+	# TODO: look at red or white wines only
